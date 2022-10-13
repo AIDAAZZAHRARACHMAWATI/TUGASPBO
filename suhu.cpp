27fde0232cbcd2c20c32835b7c6b2601547480b6
Aida Azzahra Rachmawati <aidaazzahra27@gmail.com>
2022-10-13 23:19:26 +0700

    suhu

---
+++ suhu.cpp
@@ -0,0 +1,32 @@
+//AIDA AZZAHRA RACHMAWATI
+//2100018290
+#include<iostream>							
+using namespace std;
+
+int main(){
+	double celcius, reamur, fahrenheight, kelvin;
+	int pilihan;
+		
+	cout << "=====================================================\n";
+	cout << "             PROGRAM KONVERSI SUHU\n";
+	cout << "=====================================================\n";
+	cout << endl;
+	cout << " Masukkan suhu dalam CELCIUS: ";
+	cin >> celcius;
+	cout << endl;
+	cout << " [1] REAMUR      [2] FAHRENHEIGHT    [3] KELVIN\n\n";
+	cout << " Masukkan pilihan anda: ";
+	cin >> pilihan;
+	if(pilihan == 1){
+        reamur = (4.0/5.0)*celcius;
+        cout << " Suhu dalam REAMUR: " << reamur;
+    }
+	else if (pilihan == 2) {
+        fahrenheight = (9.0/5.0)*celcius+32;
+        cout << " Suhu dalam FAHRENHEIGHT: " << fahrenheight;
+    }
+	else{
+        kelvin=celcius+273.15;
+        cout << " Suhu dalam KELVIN: " << kelvin;
+	}
+}
